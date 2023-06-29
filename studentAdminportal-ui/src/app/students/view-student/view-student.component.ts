88ceb293ce7f9e74715726cf396eafa2ea65f2da
D-Chibani <dourayd.chibani.dch@gmail.com>
2023-06-29 10:56:05 +0200

    studentAdminportal-ui - Bind model to HTML

--- studentAdminportal-ui/src/app/students/view-student/view-student.component.ts
+++ studentAdminportal-ui/src/app/students/view-student/view-student.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { StudentService } from '../student.service';
 import { ActivatedRoute } from '@angular/router';
+import { Student } from 'src/app/models/ui-models/student.model';
 
 @Component({
   selector: 'app-view-student',
@@ -9,6 +10,25 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ViewStudentComponent implements OnInit {
   studentId: string | null | undefined;
+  student: Student = {
+    id: '',
+    firstName: '',
+    lastName: '',
+    dateOfBirth: '',
+    email: '',
+    mobile: 0,
+    genderId: '',
+    profileImageUrl: '',
+    gender: {
+      id: '',
+      description: ''
+    },
+    address: {
+      id: '',
+      physicalAddress: '',
+      postalAddress: ''
+    }
+  }
 
   constructor(private studentService: StudentService,
     private readonly route: ActivatedRoute) {
@@ -22,7 +42,7 @@ export class ViewStudentComponent implements OnInit {
         if (this.studentId) {
           this.studentService.getStudent(this.studentId).subscribe(
             (successResponse) => {
-              console.log(successResponse);
+              this.student = successResponse;
             });
         }
       });
