147fc5123d3cc4215b8a37c714223d56f401e2a0
D-Chibani <dourayd.chibani.dch@gmail.com>
2023-06-30 10:08:22 +0200

    studentAdminportal-ui - Add gender List - Update Student

---
+++ studentAdminportal-ui/src/app/services/gender.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Gender } from '../models/api-models/gender.model';
+import { Student } from '../models/api-models/student.model';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GenderService {
+
+  private baseApiUrl = 'https://localhost:7230';
+  constructor(private httpClient: HttpClient) { }
+
+  getGenderList(): Observable<Gender[]> {
+    return this.httpClient.get<Gender[]>(this.baseApiUrl + '/genders')
+  }
+}
--- studentAdminportal-ui/src/app/students/view-student/view-student.component.ts
+++ studentAdminportal-ui/src/app/students/view-student/view-student.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { StudentService } from '../student.service';
 import { ActivatedRoute } from '@angular/router';
 import { Student } from 'src/app/models/ui-models/student.model';
+import { GenderService } from 'src/app/services/gender.service';
+import { Gender } from 'src/app/models/ui-models/gender.model';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-view-student',
@@ -29,9 +32,12 @@ export class ViewStudentComponent implements OnInit {
       postalAddress: ''
     }
   }
+  genderList: Gender[] = [];
 
   constructor(private studentService: StudentService,
-    private readonly route: ActivatedRoute) {
+    private readonly route: ActivatedRoute,
+    private genderService: GenderService,
+    private snackbar: MatSnackBar) {
   }
 
   ngOnInit(): void {
@@ -44,8 +50,29 @@ export class ViewStudentComponent implements OnInit {
             (successResponse) => {
               this.student = successResponse;
             });
+          this.genderService.getGenderList().subscribe(
+            (successResponse) => {
+              this.genderList = successResponse;
+            }
+          );
         }
       });
+  }
+
+  onUpdate(): void {
+    //Call Student Service to Update Student
+    this.studentService.updateStudent(this.student.id, this.student).subscribe(
+      (successResponse) => {
+        //Show noptification
+        this.snackbar.open('Student updated successefully', undefined, {
+          duration: 2000
+        });
+      },
+      (errorResponse) => {
+        // Log it
+        console.log(errorResponse);
+      }
+    );
   }
 
 }
